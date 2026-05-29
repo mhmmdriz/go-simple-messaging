58d5889e5795db2522c06be7f21221ac2ee2b83f
mhmmdriz <rizki25030@gmail.com>
2026-05-29 10:02:28 +0700

    Update User model and add User Session model

--- app/models/user.go
+++ app/models/user.go
@@ -1,18 +1,37 @@
 package models
 
 import (
+	"time"
+
 	"github.com/go-playground/validator/v10"
-	"gorm.io/gorm"
 )
 
-var validate = validator.New()
-
 type User struct {
-	gorm.Model
-	Email    string `json:"username" gorm:"unique;" validate:"required,email,min=6,max=32"`
-	Password string `json:"-" gorm:"type:text;" validate:"required,min=6"`
+	ID        uint      `json:"id" gorm:"primarykey"`
+	CreatedAt time.Time `json:"-"`
+	UpdatedAt time.Time `json:"-"`
+	Username  string    `json:"username" gorm:"unique;type:varchar(20)" validate:"required,min=6,max=32"`
+	Password  string    `json:"password,omitempty" gorm:"type:varchar(255);" validate:"required,min=6"`
+	FullName  string    `json:"full_name" gorm:"type:varchar(100);" validate:"required,min=6"`
 }
 
 func (l User) Validate() error {
-	return validate.Struct(l)
+	v := validator.New()
+	return v.Struct(l)
+}
+
+type UserSession struct {
+	ID                  uint `gorm:"primarykey"`
+	CreatedAt           time.Time
+	UpdatedAt           time.Time
+	UserID              uint      `json:"user_id" gorm:"type:int" validate:"required"`
+	Token               string    `json:"token" gorm:"type:varchar(255)" validate:"required"`
+	RefreshToken        string    `json:"refresh_token" gorm:"type:varchar(255)" validate:"required"`
+	TokenExpired        time.Time `json:"-" validate:"required"`
+	RefreshTokenExpired time.Time `json:"-" validate:"required"`
+}
+
+func (l UserSession) Validate() error {
+	v := validator.New()
+	return v.Struct(l)
 }
